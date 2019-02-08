43923cb9a334a53f4f3a69ee0eeea15039d6a42d
Micah Sandusky <micah.sandusky@ars.usda.gov>
2019-02-08 19:29:45 +0000

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     test_suite='tests',
     # tests_require=test_requirements,
     url='https://github.com/usdaarsnwrc/drhawkeye',
-    version='0.1.0',
+    version='0.2.0',
     zip_safe=False,
     scripts=[],
 )
